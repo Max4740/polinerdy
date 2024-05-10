9aa2a66fcb2d6188dbc7f313b97cc68ca923e5a0
Max4740 <sampidieu@gmail.com>
2024-05-10 16:48:58 +0700

    Done Coming soon

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,6 +1,13 @@
 export const ROUTERS ={
     USER: {
-        HOME: "",
+        HOME: "/",
         PROFILE: "thong-tin-ca-nhan",
+        DANH_MUC: "danh-muc",
+        CATCH_UP: "catch-up",
+        IRPLAYLIST: "IRPlaylist",
+        ABOUT_US: "about-us",
+        COLD_WAR: "danh-muc/cold-war",
+        CHUYEN_MUC_DAC_BIET: "danh-muc/chuyen-muc-dac-biet",
+        LICH_SU_VN: "danh-muc/lich-su-vn",
     }
 }
